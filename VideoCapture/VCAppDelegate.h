0d7b39248d41757e35ebc6f5ad167fe1bfb76ee1
Anthony Picciano <anthony.picciano@gmail.com>
2013-12-16 13:56:46 -0700

    Initial Commit

---
+++ VideoCapture/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  VideoCapture
+//
+//  Created by Anthony Picciano on 12/16/13.
+//  Copyright (c) 2013 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoCapture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoCapture
+//
+//  Created by Anthony Picciano on 12/16/13.
+//  Copyright (c) 2013 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+    }
+}
